06273a07531f97d74f1e2e8c78da1dab63b03215
Ramy D <ramy.daghstani@gmail.com>
2020-09-17 14:54:10 -0400

    no longer deleting the intermediate folder
    no longer deleting the intermediate folder after building the plugin. It seems to be required in some cases.

--- Build.bat
+++ Build.bat
@@ -3,4 +3,4 @@ set UE4DIR="C:\Program Files\Epic Games\UE_4.25"
 call %UE4DIR%\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="%CD%\UnrealInk.uplugin" -Package="%CD%\Build\UnrealInk" -Rocket
 
 robocopy "%CD%\ThirdParty" "%CD%\Build\UnrealInk\ThirdParty" /E
-rd /s /q "./Build/UnrealInk/Intermediate"
+rem rd /s /q "./Build/UnrealInk/Intermediate"
